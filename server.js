85aaf1c4442ece5b457afbfc490f3cbefd30f57b
dungvq <dung.vuquoc0102@gmail.com>
2024-09-15 00:55:50 +0700

    Fix cors

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const connection = require("./src/config/database");
 //controller
 const { getAveragePoints } = require("./src/controllers/homeController");
 
+//middleware
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 // Handle requests to the root URL
 app.get("/", getAveragePoints);
 
